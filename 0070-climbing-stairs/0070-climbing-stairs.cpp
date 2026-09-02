6e71cb40a4d65dd85d89e13806a6db88d3580100
vaidanshtaunk <taunkvaidanshh@gmail.com>
2026-09-02 22:05:24 +0530

    Time: 0 ms (100%), Space: 7.7 MB (98.9%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        
+        if (n == 1)
+            return 1;
+        
+        int prev2 = 1;
+        int prev1 = 2;
+        
+        for (int i = 3; i <= n; i++) {
+            int current = prev1 + prev2;
+            
+            prev2 = prev1;
+            prev1 = current;
+        }
+        
+        return prev1;
+    }
+};
